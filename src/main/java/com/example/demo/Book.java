aceb86392e1b76a7b461e54a9e374d0ed4e29d8d
EddyKaggia <kagzcodes@gmail.com>
2024-04-15 12:12:59 -0700

    feat: basic CRUD with jpa

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+}
